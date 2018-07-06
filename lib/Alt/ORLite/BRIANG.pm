1c4d84467701410b16c44589a9d4bb8615dadab5
brian greenfield <briang@cpan.org>
2018-07-06 21:39:51 +0100

    add lib/Alt/ORLite/BRIANG.pm

---
+++ lib/Alt/ORLite/BRIANG.pm
@@ -0,0 +1,68 @@
+package Alt::ORLite::BRIANG;
+
+our $VERSION = '0.01_01';
+
+1;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Alt::ORLite::BRIANG - ORLite updated!
+
+=head1 VERSION
+
+0.01_01
+
+=head1 SYNOPSIS
+
+    $ cpanm Alt::ORLite::BRIANG
+
+    use ORLite { ... };
+
+=head1 DESCRIPTION
+
+This is an update of L<ORLite>, that hasn't seen any updates since
+2012. Several patches from RT have been applied, as well as other
+administrative changes made.
+
+=head1 SEE ALSO
+
+=over
+
+=item * L<Alt>
+
+=item * L<ORLite>
+
+=back
+
+=head1 TO DO
+
+=over
+
+=item * Add README.md
+
+=item * Add a test for the above
+
+=item * Support for intuiting reverse relations from foreign keys
+[L<ORLite> (and RT?)]
+
+=item * Document the "L<create|ORLite/create>" and
+"L<table|ORLite/table>" parameters [L<ORLite>]
+
+=back
+
+=head1 AUTHOR
+
+Brian Greenfield <briang@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright 2018 Brian Greenfield
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
